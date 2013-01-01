361d9c0cec8f6c8092488f4b40c6a06df3d9c572
msakrani <msakrani@gmail.com>
2013-01-01 13:22:31 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Accredify::Application.config.secret_token = '9def0f5a044570e2b63773ad55eac3a409c3fa2ead530346e3856b9352a8164c48ca304dd8a3c91876d6b734c90a73ce2fbd27cd20daaab69afa7ed2434c358a'
